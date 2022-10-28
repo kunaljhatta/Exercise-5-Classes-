df35cbbb72bd1fbc62f74fccafd42be8344201ca
KunalJhatta <Kunaljhatta22@gmail.com>
2022-10-27 19:46:11 -0700

    finished exercise5

--- main.rb
+++ main.rb
@@ -2,11 +2,10 @@
 
 class LaunchDiscussionWorkflow
 
-  def initialize(discussion, host, participants_email_string)
+  def initialize(discussion, host, participants)
     @discussion = discussion
     @host = host
-    @participants_email_string = participants_email_string
-    @participants = []
+    @participants = participants
   end
 
   # Expects @participants array to be filled with User objects
@@ -20,6 +19,15 @@ class LaunchDiscussionWorkflow
       end
     end
   end
+  #....
+end
+
+class GenerateParticipant
+
+  def initialize(participants_email_string)
+    @participants_email_string = participants_email_string
+    @participants = []
+  end
 
   def generate_participant_users_from_email_string
     return if @participants_email_string.blank?
@@ -27,16 +35,15 @@ class LaunchDiscussionWorkflow
       User.create(email: email_address.downcase, password: Devise.friendly_token)
     end
   end
-
-  # ...
-
+  #....
 end
 
 
 discussion = Discussion.new(title: "fake", ...)
 host = User.find(42)
-participants = "fake1@example.com\nfake2@example.com\nfake3@example.com"
+participants = GenerateParticipant.new("fake1@example.com\nfake2@example.com\nfake3@example.com")
+participants.generate_participant_users_from_email_string
 
 workflow = LaunchDiscussionWorkflow.new(discussion, host, participants)
 workflow.generate_participant_users_from_email_string
-workflow.run
+workflow.run 
